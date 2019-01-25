db5618715705845f25835df2cfb4a84aa195f31b
karthirajan <kjekarthi@gmail.com>
2019-01-25 12:45:46 +0530

    modified

---
+++ src/org/selenium/Hello.java
@@ -0,0 +1,15 @@
+package org.selenium;
+
+public class Hello {
+	public void test3() {
+		System.out.println("3rd test");
+
+	}
+	public void test4() {
+		System.out.println("4th test");
+
+	}
+		
+	}
+
+
--- src/org/selenium/Sample.java
+++ src/org/selenium/Sample.java
@@ -1,12 +1,12 @@
 package org.selenium;
 
 public class Sample {
-	public void test1() {
+	public void test3() {
 		System.out.println("1st class");
 
 	}
 	public void test2() {
-		System.out.println("2nd class");
+		System.out.println("second class");
 
 	}
 
