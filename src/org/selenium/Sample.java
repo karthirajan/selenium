7d5c768941f653c4d38fe20b859b358cf41d325d
karthirajan <kjekarthi@gmail.com>
2019-01-25 12:10:59 +0530

    initial class

---
+++ src/org/selenium/Sample.java
@@ -0,0 +1,13 @@
+package org.selenium;
+
+public class Sample {
+	public void test1() {
+		System.out.println("1st class");
+
+	}
+	public void test2() {
+		System.out.println("2nd class");
+
+	}
+
+}
